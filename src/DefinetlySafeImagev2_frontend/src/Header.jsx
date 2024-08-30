3619cabd86565ce2bf6ab4c85efc83858e94fd97
wpram45 <wpram734@gmail.com>
2024-08-31 00:30:42 +0300

    progressbarlar eklenmeli(gonderirken ve alirken) ve resim gonderildikten sonra sayfa refresh edilmeli

--- src/DefinetlySafeImagev2_frontend/src/Header.jsx
+++ src/DefinetlySafeImagev2_frontend/src/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import  "./styles/headerStyle.css"
 export default function Header() {
   return (
 
-    <div style={{marginTop:"40px"}}>
+    <div style={{marginTop:"40px",display:"flex",width:"100%",flexDirection:"column"}}>
     <img src="/logo2.svg" alt="DFINITY logo" />
+    <h4 className='headerText'>
+    "Definitely Safe Image Project" is associated with Şerif Geyik.
+    </h4>
     </div>
 
   )
